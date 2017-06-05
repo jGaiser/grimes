1997cd9ff52dc386567c4ece42206ac93f6824fb
Jeremiah Gaiser <jeraiser@gmail.com>
2017-06-05 15:13:55 -0600

    Initial commit

--- run.py
+++ run.py
@@ -9,18 +9,28 @@ app = Flask(__name__);
 @app.route('/<network_id>')
 @app.route('/<network_id>/')
 def home(network_id=None):
-	"""
-	    Gather important data to send to client side with home template
-	"""
-    cx_data = json.dumps(get_network_cx(network_id))
-    return render_template('home.html', cx_data = cx_data) 
+    """
+        Gather important data to send to client side with home template
+    """
+
+    cx_data = json.dumps(get_local_cx("Criz Network Proteins.cx"))
+    return render_template('home.html', cx_data = cx_data)
+
+
+def get_local_cx(filename):
+    """
+        Retrieve CX data from local cx file
+    """
+
+    cx_file = open(filename, 'r')
+    return cx_file.read().decode('utf8')
 
 
 def get_network_cx(network_id):
-	"""
-	    Get CX data from public NDEx server
-	    TODO: Provide interface for different methods, i.e. 'metadata' instead of 'network' 
-	"""
+    """
+        Get CX data from public NDEx server
+        TODO: Provide interface for different methods, i.e. 'metadata' instead of 'network' 
+    """
 
     endpoint   = "http://www.ndexbio.org/v2"
     method     = 'network'
--- static/main.js
+++ static/main.js
@@ -1,8 +1,20 @@
-function cyMain(){
+function CytoscapeObject(){
+
+  var edgeTypes = [];
 
   function randomColor(){
-    var backgroundColors = ['red', 'blue', 'green', 'purple', 'yellow', 'pink', 'black', 'orange'];
-    return backgroundColors[Math.floor(Math.random()*backgroundColors.length)]
+
+    function randomValue(){
+      return Math.floor(Math.random()*155 + 100);
+    }
+
+    var returnString = "rgb("
+
+    for (var i = 0; i < 3; i++) { 
+      returnString += randomValue() + ",";
+    }
+
+    return returnString.slice(0, -1) + ")";
   } 
 
   // Initialize cytoscape.js object 
@@ -18,7 +30,7 @@ function cyMain(){
     ]
   });
 
-  function get_nodes(){
+  function fetch_nodes(){
 
     // Iterates through cx_data and adds all nodes to cy's 'node' group 
 
@@ -33,22 +45,27 @@ function cyMain(){
         })      
       }  
     })
-
   }
 
-  function get_edges(){
+  function fetch_edges(){
 
     // Iterates through cx_data and adds all edges to cy's 'edges' group 
 
     cx_data.forEach(function(elem){
       if(Object.keys(elem)[0] == 'edges'){
         elem.edges.forEach(function(edge){
+
+          if(edgeTypes.indexOf(edge.i) == -1){
+            edgeTypes.push(edge.i);
+          }
+
           cy.add({
             group: "edges",
             data: {
               id: edge["@id"],
               source: edge.s,
-              target: edge.t 
+              target: edge.t,
+              type: edge.i 
             }  
           })   
         })   
@@ -56,14 +73,39 @@ function cyMain(){
     })
   }
 
-  get_nodes();
-  get_edges();
+  function styleGraph(){
 
-  cy.layout({ 
-    name: 'concentric'
-  }).run();
+    edgeTypes.forEach(function(type){
+      cy.style().selector('edge[type="'+ type +'"]').style({'line-color': randomColor()});
+    })
+
+    cy.style().update();
+  }
+
+  function init(){
+    fetch_nodes();
+    fetch_edges();
+
+    cy.layout({ 
+      name: 'cose'
+    }).run();
+
+    styleGraph();
+  }
+
+  init();
 };
 
+function formatViewport(){
+  // Sets cytoscape viewport element to appropriate height
+
+  var viewport = document.getElementById('cyViewport'),
+      windowHeight = window.innerHeight;
+
+  viewport.style.height = windowHeight - 250 + 'px';
+}  
+
 window.onload = function(){ 
-  cyMain(); 
+  formatViewport();
+  var cy = new CytoscapeObject();
 }
